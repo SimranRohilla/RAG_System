031affb8ae0a58e512e66e9aea29fbc3f5f6c751
SimranRohilla <122539195+SimranRohilla@users.noreply.github.com>
2025-02-18 22:28:40 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://rag-system-1.onrender.com"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
